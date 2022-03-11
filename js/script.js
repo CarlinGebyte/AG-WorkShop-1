3e80a0f42ed384ac620949565530fbe81f2a20b4
Yessith Estiven Herrera Sanjuan <yessith.hs@gmail.com>
2022-03-11 17:00:34 -0500

    Add layout header

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const menu = document.querySelector('#menu')
+const dropdown = document.querySelector('#burger')
+const line = document.querySelector('.prince-navLine')
+const link = document.querySelectorAll('.prince-navMenu-link')
+
+/* SHOW MENU */
+dropdown.addEventListener('click', () => {
+	menu.classList.toggle('show')
+	line.classList.toggle('active')
+})
+
+/* HIDDEN MENU */
+link.forEach((n) =>
+	n.addEventListener('click', () => {
+		menu.classList.remove('show')
+		line.classList.remove('active')
+	})
+)
